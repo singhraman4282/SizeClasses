538e7ee473e20ad056d7315476d49807e83b8d9b
Raman Singh <rami88@ymail.com>
2018-05-28 14:21:47 -0700

    Added size classes to three labels

---
+++ sizeClasses/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sizeClasses
+//
+//  Created by Raman Singh on 2018-05-28.
+//  Copyright © 2018 Raman Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
